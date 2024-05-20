541e77017f25ccb5c92c231a55927da865d98af4
titimechon <he.domingo@duocuc.cl>
2024-05-20 19:07:58 -0400

    iniciar sesión

--- ferremas/views.py
+++ ferremas/views.py
@@ -1,8 +1,8 @@
 from django.contrib import messages
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
-from django.contrib.auth import login, logout
+from django.contrib.auth import login, logout, authenticate
 from django.db import IntegrityError
 from . models import Producto
 
@@ -21,14 +21,14 @@ def registro(request):
         'form': UserCreationForm
         })
     else:
-        if request.POST ['password1'] == request.POST['password1']:
+        if request.POST ['password1'] == request.POST['password2']:
             try:
                 user = User.objects.create_user(
                     username=request.POST['username'],
                     password=request.POST['password1'])
                 user.save()
                 login(request, user)
-                return redirect('signin')
+                return redirect('inicio')
             except IntegrityError:
                 return render(request, 'registro.html', {
                     'form': UserCreationForm,
@@ -39,9 +39,17 @@ def registro(request):
             'error': 'contraseñas no coinciden'
         })
 
-def singin(request):
-    return render(request, 'login.html')
-
+def signin(request):
+    if request.method == 'GET':
+        return render(request, 'signin.html',{'form':AuthenticationForm})
+    else:
+        user = authenticate(request, username=request.POST['username'], password = request.POST['password'])
+        if user is None:
+            return render (request, 'signin.html', {'form':AuthenticationForm, 'error':'usuario o contraseña incorrecto'})
+        else:
+            login(request, user)
+            return redirect('inicio')
+        
 def agregarproducto(request):
     if request.method == 'POST':
         # Procesar los datos del formulario de agregar producto
